3f84ecf100aabbf402ac6f55f25d3cd89bf42fe4
mariaromeromartin <100472330@alumnos.uc3m.es>
2024-03-16 18:56:04 +0100

    Cmabios en fichero hotelmanager

--- src/main/python/UC3MTravel/HotelManager.py
+++ src/main/python/UC3MTravel/HotelManager.py
@@ -1,35 +1,37 @@
 import json
-from .HotelManagementException import HotelManagementException
-from .HotelReservation import HotelReservation
+
+from .HotelManagementException import HOTELMANAGMENTEXCEPTION
+
+from .HotelReservation import HOTELRESERVATION
 
 class HOTELMANAGER:
     def __init__( self ):
         pass
 
-    def validatecreditcard( self, x ):
+    def VALIDATECREDITCARD( self, x ):
         # PLEASE INCLUDE HERE THE CODE FOR VALIDATING THE GUID
         # RETURN TRUE IF THE GUID IS RIGHT, OR FALSE IN OTHER CASE
         return True
 
-    def ReaddatafromJSOn( self, fi):
+    def READDATAFROMJSON( self, strFi ):
 
         try:
-            with open(fi) as f:
-                DATA = json.load(f)
+            with open( strFi ) as f:
+                strDATA = json.load( f )
         except FileNotFoundError as e:
-            raise HotelManagementException("Wrong file or file path") from e
+            raise HOTELMANAGMENTEXCEPTION("Wrong file or file path") from e
         except json.JSONDecodeError as e:
-            raise HotelManagementException("JSON Decode Error - Wrong JSON Format") from e
+            raise HOTELMANAGMENTEXCEPTION("JSON Decode Error - Wrong JSON Format") from e
 
 
         try:
-            c = DATA["CreditCard"]
-            p = DATA["phoneNumber"]
-            req = HotelReservation(IDCARD="12345678Z",creditcardNumb=c,nAMeAndSURNAME="John Doe",phonenumber=p,room_type="single",numdays=3)
+            c = strData["CreditCard"]
+            p = strData["phoneNumber"]
+            req = HOTELRESERVATION(IDCARD="12345678Z",creditcardNumb=c,nAMeAndSURNAME="John Doe",phonenumber=p,room_type="single",numdays=3)
         except KeyError as e:
-            raise HotelManagementException("JSON Decode Error - Invalid JSON Key") from e
-        if not self.validatecreditcard(c):
-            raise HotelManagementException("Invalid credit card number")
+            raise HOTELMANAGMENTEXCEPTION("JSON Decode Error - Invalid JSON Key") from e
+        if not self.VALIDATECREDITCARD(c):
+            raise HOTELMANAGMENTEXCEPTION("Invalid credit card number")
 
         # Close the file
         return req
