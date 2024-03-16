af639ee76ac9eea21f417b47889efe7cbab3a038
mariaromeromartin <100472330@alumnos.uc3m.es>
2024-03-16 22:37:29 +0100

    actualizacion del codigo

--- src/main/python/UC3MTravel/hotelmanager.py
+++ src/main/python/UC3MTravel/hotelmanager.py
@@ -6,6 +6,7 @@ from .HotelManagementException import HOTELMANAGMENTEXCEPTION
 
 from .HotelReservation import HOTELRESERVATION
 
+
 class HOTELMANAGER:
     def __init__( self ):
         pass
@@ -25,15 +26,14 @@ class HOTELMANAGER:
         except json.JSONDecodeError as e:
             raise HOTELMANAGMENTEXCEPTION("JSON Decode Error - Wrong JSON Format") from e
 
-
         try:
-            strC = strData["CreditCard"]
-            strP = strData["phoneNumber"]
-            req = HOTELRESERVATION(IDCARD="12345678Z",creditcardNumb=strC,nAMeAndSURNAME="John Doe",phonenumber=strP,room_type="single",numdays=3)
+            strC = strData[ "CreditCard" ]
+            strP = strData[ "phoneNumber" ]
+            req = HOTELRESERVATION( IDCARD="12345678Z", creditcardNumb=strC, nAMeAndSURNAME="John Doe",
+                                    phonenumber=strP, room_type="single", numdays=3 )
         except KeyError as e:
             raise HOTELMANAGMENTEXCEPTION("JSON Decode Error - Invalid JSON Key") from e
-        if not self.VALIDATECREDITCARD(c):
-            raise HOTELMANAGMENTEXCEPTION("Invalid credit card number")
+        if not self.VALIDATECREDITCARD(strC): raise HOTELMANAGMENTEXCEPTION("Invalid credit card number")
 
         # Close the file
         return req
