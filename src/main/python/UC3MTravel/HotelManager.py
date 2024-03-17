d403e17559d5975e3354f49744a99cc27c468642
mariaromeromartin <100472330@alumnos.uc3m.es>
2024-03-17 02:02:11 +0100

    VERSION ACTUALIZADA CON FUNCION VALIDATECARD

--- src/main/python/UC3MTravel/HotelManager.py
+++ src/main/python/UC3MTravel/HotelManager.py
@@ -11,10 +11,26 @@ class HOTELMANAGER:
     def __init__( self ):
         pass
 
-    def VALIDATECREDITCARD( self, x ):
-        # PLEASE INCLUDE HERE THE CODE FOR VALIDATING THE GUID
-        # RETURN TRUE IF THE GUID IS RIGHT, OR FALSE IN OTHER CASE
-        return True
+    def VALIDATECREDITCARD(self, strCreditCardNum:str ) ->bool:
+            #Comprobar la longitud de la tarjeta de credito
+            if (len(strCreditCardNum) != 16):
+                return False
+            intChecksum = int(strCreditCardNum[-1])
+            #Metemos los numeros en una lista
+            lstNumeros = [int(intNum) for intNum in strCreditCardNum[:-1]]
+
+            # Duplicamos los numeros en las posiciones pares empezando por 1
+            lstNumeros = [lstNumeros[i] * 2 if i % 2 == 0 else lstNumeros[i] for i in range(len(lstNumeros))]
+
+            # Sumamos los digitos de los numeros mayores a 9
+            lstNumeros = [sum([int(num) for num in str(lstNumeros[i])]) if lstNumeros[i] > 9 else lstNumeros[i] for i in
+                       range(len(lstNumeros))]
+
+            # Sumamos todos los numeros y calculamos el checksum teorico
+            intSuma = sum(lstNumeros)
+            intChecksumTeorica = intSuma * 9 % 10
+
+            return intChecksum == intChecksumTeorica
 
     def READDATAFROMJSON( self, strFi ):
 
