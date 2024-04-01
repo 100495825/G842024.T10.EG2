7ca8df6cec41162f3297d8caa50fe5470c1f258d
mariaromeromartin <100472330@alumnos.uc3m.es>
2024-04-01 16:55:10 +0200

    Actualizaciones nuevas metodo 2

--- src/main/python/UC3MTravel/HotelStay.py
+++ src/main/python/UC3MTravel/HotelStay.py
@@ -4,7 +4,7 @@
 import hashlib
 
 class HOTELSTAY():
-    def __init__(self, strIdCard, strLocalizer, intNumdays, strRoomType):
+    def __init__(self, strIdCard, strLocalizer, intNumDays, strRoomType):
         self.__strAlg = "SHA-256"
         self.__strRoomType = strRoomType
         self.__strIdCard = strIdCard
@@ -13,7 +13,7 @@ class HOTELSTAY():
         self.__dateArrival = dateJustNow
         #timestamp is represented in seconds.miliseconds
         #to add the number of days we must express numdays in seconds
-        self.__dateDeparture = self.__dateArrival + (intNumdays * 24 * 60 * 60)
+        self.__dateDeparture = self.__dateArrival + (intNumDays * 24 * 60 * 60)
 
     def __SIGNATURESTRING(self):
         """Composes the string to be used for generating the key for the room"""
